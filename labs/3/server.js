10858b0ca33bc15eec843621f72c15d40239d245
Ephraim Flores <ephraim.flores@ymail.com>
2025-09-27 22:41:42 -0700

    Add paths for writing/reading from a file

--- labs/3/server.js
+++ labs/3/server.js
@@ -3,6 +3,7 @@
 const http = require("http");
 const url = require("url");
 const utils = require("./modules/utils.js");
+const fs = require("fs");
 
 http.createServer(function (req, res) {
   const q = url.parse(req.url, true);
@@ -13,6 +14,38 @@ http.createServer(function (req, res) {
     const name = qdata.name || "Guest";
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(utils.getDate(name));
+    
+  } else if (path === "/COMP4537/labs/3/writeFile/") {
+    const text = qdata.text;
+    if (!text) {
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end("<p style='color:red;'>Missing ?text= paramater</p>");
+        return;
+    }
+
+    fs.appendFile("file.txt", text + "\n", (err) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/html" });
+            res.end("<p style='color:red;'>Error writing file</p>");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(`<p style='color:blue;'>Appended "${text}" to file.txt</p>`);
+        }
+    });
+
+  } else if (path.startsWith("/COMP4537/labs/3/readFile/")) {
+    const filename = path.replace("/COMP4537/labs/3/readFile/", "");
+
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(`<p style='color:red;'>404 Not Found: ${filename}</p>`);
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(`<pre style="color:blue;">${data}</pre>`);
+        }
+    });
+
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>404 Not Found</h1>");
