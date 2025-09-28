bd847474ebba18b87c85a71a3d49c3623c8086c1
Ephraim Flores <ephraim.flores@ymail.com>
2025-09-27 18:02:28 -0700

    Modify server.js

--- labs/3/server.js
+++ labs/3/server.js
@@ -6,11 +6,19 @@ const utils = require("./modules/utils.js");
 
 http.createServer(function (req, res) {
   const q = url.parse(req.url, true);
+  const path = q.pathname;
   const qdata = q.query;
-  const name = qdata.name || "Guest";
 
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(utils.getDate(name));
+  if (path === "/COMP4537/labs/3/getDate/") {
+    const name = qdata.name || "Guest";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(utils.getDate(name));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 Not Found</h1>");
+  }
+
+  
 }).listen(8888);
 
-console.log("Server running at http://localhost:8888/");
+console.log("Server running at http://localhost:8888/COMP4537/labs/3/getDate/");
